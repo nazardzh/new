6c8dfa657b47881d4ad976693849e99aa7eb2b0c
nazardzh <nazar.dzhuruk@gmail.com>
2020-04-29 17:18:19 +0200

    lesson_3

--- main/src/main.java
+++ main/src/main.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class main {
+    public static void main(String...args){
 
+        int a;
+        Scanner num = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        a = num.nextInt();
+        if (a == 10){
+            System.out.println("Hello name");
+        }else if(a == 5){
+            System.out.println("Hello noname");
+        }else if (a == 7){
+            System.out.println("how?");
+        }
+
+    }
 }
