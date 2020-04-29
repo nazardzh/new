46e5b595c29311acdc52bd28107a7333a859f558
nazardzh <nazar.dzhuruk@gmail.com>
2020-04-29 19:10:39 +0200

    array lesson

---
+++ main/src/arrays.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class arrays {
+    public static void main(String... args){
+//       int [] local;
+//       int a;
+//       Scanner num = new Scanner(System.in);
+//       System.out.print("Enter number of arrays: ");
+//       a = num.nextInt();
+//       local = new int[a];
+//       for (int i = 0; i < a; i++){
+//           System.out.print("Enter a [" + i +"]=");
+//           local[i] = num.nextInt();
+//       }
+
+
+        //new code remove '//'
+//        Scanner num = new Scanner(System.in);
+//        int a;
+//        System.out.println("Enter number of array: ");
+//        a = num.nextInt();
+//        String[] days = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+//        System.out.println("day is: " + days[a - 1]);
+
+    }
+}
