528a1a26b0c5337861e8a67ce4b97f857d0aec0b
nazardzh <nazar.dzhuruk@gmail.com>
2020-05-17 14:32:52 +0200

    homework_oop

--- main/src/homework_oop/Main.java
+++ main/src/homework_oop/Main.java
@@ -7,8 +7,9 @@ public class Main {
         System.out.println("Name: " + emploee1.getName());
         System.out.println("ID: " + emploee1.getId());
         System.out.println("Previous salary: " + emploee1.getSalary());
-        System.out.println("Raise salary (salary * percent): " + emploee1.raiseSalary(12));
+        System.out.println("Raise salary (salary * percent): " + emploee1.raiseSalary(5));
         System.out.println("Annual salary: " + emploee1.getAnnualSalary());
+        System.out.println(emploee1.toString());
 
 
 
