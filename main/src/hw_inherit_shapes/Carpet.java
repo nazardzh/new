c3960891e79d9faae0191a131ea9fcca49fcf00d
nazardzh <nazar.dzhuruk@gmail.com>
2020-05-24 14:17:30 +0200

    Homework 1,2,3 (not for 100%)

--- main/src/hw_inherit_shapes/Carpet.java
+++ main/src/hw_inherit_shapes/Carpet.java
@@ -1,13 +1,14 @@
 package hw_inherit_shapes;
 
 public class Carpet {
+    public double size;
     Shape shape1 = new Circle();
     Shape shape2 = new Square();
     Shape shape3 = new Rectangle();
     Shape[] shapes = {shape1, shape2, shape3};
 
     public double getSize() {
-       return shape1.getArea() + shape2.getArea() + shape3.getArea();
+        return shape1.getArea() + shape2.getArea() + shape3.getArea();
     }
 }
 
--- main/src/hw_inherit_shapes/mainShape.java
+++ main/src/hw_inherit_shapes/mainShape.java
@@ -3,7 +3,7 @@ package hw_inherit_shapes;
 public class mainShape {
     public static void main(String[] args){
         Carpet capets = new Carpet();
-//        System.out.println(circle1.getSize());
+     System.out.println(capets.getSize());
 
 
 
